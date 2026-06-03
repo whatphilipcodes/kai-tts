012e1f0c82e88908f25c73020c5f5153ba9942b9
whatphilipcodes <gerdes.philip@gmail.com>
2026-06-03 13:05:44 +0200

    feat: ilja

--- dev/server.py
+++ dev/server.py
@@ -5,12 +5,13 @@ from pathlib import Path
 HOST = "127.0.0.1"
 PORT = 5005
 
-ref = Path(__file__).resolve().parent.parent / "resources" / "source-philip.wav"
+ref = Path(__file__).resolve().parent.parent / "resources" / "source-ilja.wav"
 
+ref_text_ilja = "Das Scheinwerferlicht bricht sich in der Stille des großen Hauses, während der Vorhang langsam beiseite gleitet. In diesem flüchtigen Moment zwischen Realität und Illusion verschmelzen die Worte des Dramas mit der Präsenz des Schauspielers, um das Publikum in eine Welt voll dramatischer Tiefe und ungeahnter Leidenschaft zu entführen."
 ref_text_philip = "Ich muss mal ausprobieren wie meine Stimme jetzt durch dieses System klingt weil das habe ich nämlich actually noch gar nicht gemacht und das sind ja jetzt doch ein paar Neuerungen dazu gekommen insofern hat es ja vielleicht 'nen überraschenden Effekt."
+
 ref_text_rt = "Wir wohnten der Generalprobe unserer eigenen Beerdigung bei - Unglücklicherweise erlebt dieses Abenteuer das allen gemeinsam ist jeder allein."
 ref_text_kinski = "Nein, er hat nicht gesagt halt die Schnauze. Er hat eine Peitsche genommen und hat ihm in die Fresse gehauen!"
-ref_text_stinken = "Sie haben gesagt ich stinke."
 
 
 def main():
@@ -49,7 +50,7 @@ def main():
                 for chunk in model.generate_streaming(
                     text=user_input,
                     prompt_wav_path=ref,
-                    prompt_text=ref_text_philip,
+                    prompt_text=ref_text_ilja,
                     reference_wav_path=ref,
                 ):
                     data = chunk.tobytes()

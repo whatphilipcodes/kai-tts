55b0c1d57d18f6655fc007a3c1f06555bcb939df
whatphilipcodes <gerdes.philip@gmail.com>
2026-07-29 15:33:37 +0200

    refactor: test node connection

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from src.kai_tts.config_tts import settings_tts
 from kai_shared.utils.logger import get_logger, setup_logging
 from kai_shared.io.node import PipelineNode
+import asyncio
 
 setup_logging()
 logger = get_logger(__name__)
 
 
-def main() -> None:
+async def main() -> None:
     app_node = PipelineNode(config=settings_tts.shared)
-    app_node.run()
+    await app_node.run()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
